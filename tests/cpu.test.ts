5d5f81a32c197d044b1a885ca85a124c3808a20a
hork71 <ppost96@yahoo.com>
2020-06-17 13:50:51 +0000

    expanded tests

--- tests/cpu.test.ts
+++ tests/cpu.test.ts
@@ -12,17 +12,25 @@ const createCPU = (sizeInBytes: number = 256) => {
 };
 
 Deno.test("CPU.getRegister fetches existing register names", function (): void {
-  const cpu = createCPU();
+  const cpu = createCPU(512);
 
   assertEquals(cpu.getRegister('ip'), 0);
-  assertEquals(cpu.getRegister('sp'), 254);
-  assertEquals(cpu.getRegister('fp'), 254);
+  assertEquals(cpu.getRegister('r1'), 0);
+  assertEquals(cpu.getRegister('r2'), 0);
+  assertEquals(cpu.getRegister('r3'), 0);
+  assertEquals(cpu.getRegister('r4'), 0);
+  assertEquals(cpu.getRegister('r5'), 0);
+  assertEquals(cpu.getRegister('r6'), 0);
+  assertEquals(cpu.getRegister('r7'), 0);
+  assertEquals(cpu.getRegister('r8'), 0);
+  assertEquals(cpu.getRegister('sp'), 510);
+  assertEquals(cpu.getRegister('fp'), 510);
 });
 
 Deno.test("CPU.getRegister throws on non-existing register names", function (): void {
   const cpu = createCPU();
 
-  assertThrows((): void => {
+  assertThrows(function (): void {
     cpu.getRegister('Does not exist');
   });
 });
